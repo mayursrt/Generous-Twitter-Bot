a34362296bbffae7bea40175821dd62859f2c64f
Mayur Machhi <mayurmachhi19@gmail.com>
2020-12-20 15:46:11 +0530

    initial

--- GenerousTwitterBot.py
+++ GenerousTwitterBot.py
@@ -1,8 +1,8 @@
 import tweepy
 import time
 
-auth = tweepy.OAuthHandler('8pScqXU7kEddPBwQIIAB7dEbc', 'Ai3vmKgPZSijOQEush2MZA3oiiezSQOjdjJ9uWyvFgVxbxIlzI')
-auth.set_access_token('810392409834192896-bo5Oz6zPSETgzN0ASE2SRikuete1dT4', 'xiCH5jS2lIyHYUioiniz7S9mtiYXvgSbZLSyYYJmKC4UH')
+auth = tweepy.OAuthHandler('', '')
+auth.set_access_token('', '')
 
 api = tweepy.API(auth)
 user = api.me()
